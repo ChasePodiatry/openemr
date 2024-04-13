691ab24a942bdfa408ff0a71b5752cbaf3efd409
Tom Mychost <tom@mycho.st>
2024-04-13 16:32:18 +0100

    Add health_status.php from live

---
+++ health_status.php
@@ -0,0 +1,33 @@
+<?php
+ini_set('exit_on_timeout', true);
+ini_set('max_execution_time', 5);
+ini_set('mysql.connect_timeout', 5);
+
+$ignoreAuth=true;
+include_once("interface/globals.php");
+include_once("$srcdir/sql.inc");
+
+$sql = "SELECT gl_value FROM globals WHERE gl_name = 'support_phone_number';";
+$res = sqlQueryNoLog($sql);
+$phone = $res['gl_value'];
+
+$error = false;
+
+if (!$phone || !$config) {
+    http_response_code(500);
+    $error = true;
+}
+
+?>
+<html>
+
+<body>
+<h1><?php echo $GLOBALS["openemr_name"] ?> System Status</h1>
+Summary: <?php echo $error ? 'not-ok' : "ok" ?>
+<ul>
+    <li>Configured: <?php echo $config ? "yes" : "no" ?></li>
+    <li>Support phone: <?php echo $phone ? $phone : "error" ?></li>
+</ul>
+</body>
+
+</html>
