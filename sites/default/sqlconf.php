5ff17abf81f7030da7a52365f8846cf07828d0e4
Tom Mychost <tom@mycho.st>
2024-04-13 18:01:03 +0100

    Add fetching database config from environment variables

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -6,11 +6,11 @@
 global $disable_utf8_flag;
 $disable_utf8_flag = false;
 
-$host	= 'localhost';
-$port	= '3306';
-$login	= 'openemr';
-$pass	= 'openemr';
-$dbase	= 'openemr';
+$host	= isset($_ENV['MYSQL_HOST']) ? $_ENV['MYSQL_HOST'] : 'localhost';
+$port	= isset($_ENV['MYSQL_PORT']) ? $_ENV['MYSQL_PORT'] : '3306';
+$login	= isset($_ENV['MYSQL_LOGIN']) ? $_ENV['MYSQL_LOGIN'] : 'openemr';
+$pass	= isset($_ENV['MYSQL_PASS']) ? $_ENV['MYSQL_PASS'] : 'openemr';
+$dbase	= isset($_ENV['MYSQL_DBASE']) ? $_ENV['MYSQL_DBASE'] : 'openemr';
 
 $sqlconf = array();
 global $sqlconf;
